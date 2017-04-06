a5cfb4384392a5c9e7daa3dc108fe822696708ca
randomcoww <etherblimp@gmail.com>
2017-04-05 23:25:02 -0700

    fix

--- libraries/node_data.rb
+++ libraries/node_data.rb
@@ -5,13 +5,18 @@ module NodeData
     require 'ipaddr'
 
     ## get host IP by subnet - chef ipaddress doesn't always have the IP I want
-    def self.subnet_ipv4(subnet)
+    def self.subnet_ipv4(network)
       subnet_ips = []
+      begin
+        n = IPAddr.new(network)
 
-      Socket.ip_address_list.each do |e|
-        if e.ipv4_private? && subnet.include?(e.ip_address)
-          subnet_ips << e.ip_address
+        Socket.ip_address_list.each do |e|
+          if e.ipv4_private? && n.include?(e.ip_address)
+            subnet_ips << e.ip_address
+          end
         end
+      rescue => e
+        Chef::Log.error("Failed to parse node IP: #{e}")
       end
       return subnet_ips
     end
