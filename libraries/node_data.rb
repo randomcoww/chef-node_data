bc2a44c06082be5537b97afabb022e0dc133b011
randomcoww <etherblimp@gmail.com>
2017-05-05 22:55:34 -0700

    Revert "allow query by options"
    This reverts commit 348a6f9fd2e9e386c7de9b506e2d68abb280ad12.

--- libraries/node_data.rb
+++ libraries/node_data.rb
@@ -5,16 +5,15 @@ module NodeData
     require 'ipaddr'
 
     ## get host IP by subnet - chef ipaddress doesn't always have the IP I want
-    def self.subnet_ipv4(opts)
+    def self.subnet_ipv4(network)
       subnet_ips = []
       begin
-        opts[:network] = IPAddr.new(opts[:network]) if !opts[:network].nil?
+        n = IPAddr.new(network)
 
         Socket.ip_address_list.each do |e|
-          next unless !opts[:network].nil? || opts[:network].include?(e.ip_address)
-          next unless !opts[:private] || e.ipv4_private?
-
-          subnet_ips << e.ip_address
+          if e.ipv4_private? && n.include?(e.ip_address)
+            subnet_ips << e.ip_address
+          end
         end
       rescue => e
         Chef::Log.error("Failed to parse node IP: #{e}")
